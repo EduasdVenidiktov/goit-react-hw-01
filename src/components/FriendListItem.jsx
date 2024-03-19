639ebf02c95309b1f44f4a8ee7d411abdf48a15e
EduardVenidiktov <edven7777777@gmail.com>
2024-03-19 17:10:59 +0200

    покращення коду

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -4,6 +4,7 @@ const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
       <img className={css.friendImg} src={avatar} alt="Avatar" width="48" />
+      <p className={css.friendName}>{name}</p>
       <p className={isOnline ? css.online : css.offline}>
         {isOnline ? 'Online' : 'Offline'}
       </p>
